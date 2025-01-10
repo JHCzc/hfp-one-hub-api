7655bff6c8db8104fdffe67e6235cce624d6b048
jhczc <zeeshanc@helerfoods.com>
2025-01-10 16:36:25 +0000

    Update the email credentials

--- controller/emailController.js
+++ controller/emailController.js
@@ -5,12 +5,12 @@ const path = require("path");
 
 // SMTP transporter without authentication
 const transporter = nodemailer.createTransport({
-  host: "smtp.office365.com", // Replace with your SMTP server's hostname
+  host: process.env.host, // Replace with your SMTP server's hostname
   port: 587, // Common port for unauthenticated SMTP
   secure: false, // Set to true if using port 465 with SSL
   auth: {
-    user: "asn-sender@helerfoods.com",
-    pass: "JHCas73.059!",
+    user: process.env.emailUser,
+    pass: process.env.emailPass,
   },
  
 });
@@ -26,7 +26,7 @@ const sendEmail = async (req, res) => {
   }
 
   const mailOptions = {
-    from: "asn-sender@helerfoods.com",
+    from: process.env.emailFrom,
     to: recipient,
     subject: subject,
     text: message,
